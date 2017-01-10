e7bd036f7835513a772c963743ef976b2332734e
Joseph Tsai <josephkt@uw.edu>
2017-01-10 14:36:06 -0800

    Complete exercises 1-3

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -3,14 +3,26 @@
 # Define a function `AddThree` that takes a single argument and
 # returns a value 3 greater than that input
 
+AddThree <- function(given.number) {
+	return(given.number + 3)
+}
+
 
 # Create a variable `ten` that is the result of passing 7 to your `AddThree` function
 
+ten <- AddThree(7)
 
 # Define a function `ImperialToMetric` that takes in two arguments: a number of feet
 # and a number of inches
 # The function should return the total length in meters
 
-
+ImperialToMetric <- function(num.feet, num.inches){
+	inches.to.feet <- (num.inches / 12)
+	total.num.feet <- (inches.to.feet + num.feet)
+	return (total.num.feet * 3.28084)
+	 
+}
 # Create a variable `height.in.meters` by passing your height in imperial to the
 # `ImperialToMetric` function
+
+height.in.meters <- ImperialToMetric(5, 10)
--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,13 +4,36 @@
 # the sentence of the form
 #   "The difference in length is N"
 
+CompareLength <- function(character.string.1, character.string.2) {
+	length.string.1 <- nchar(character.string.1)
+	length.string.2 <- nchar(character.string.2)
+	length.difference <- (length.string.1 - length.string.2)
+	return(paste("The difference in length is", "length.difference"))
+}
+
 
 # Pass two strings of different lengths to your `CompareLength` function
 
+CompareLength("cat", "dogs")
 
 # Define a function `DescribeDifference` that will return one of the following statements:
 # "Your first string is longer by N characters"
 # "Your second string is longer by N characters"
 
+DescribeDifference <- function(character.string.1, character.string.2) {
+	length.string.1 <- nchar(character.string.1)
+	length.string.2 <- nchar(character.string.2)
+	
+	if(length.string.1 > length.string.2) {
+		length.difference <- length.string.1 - length.string.2
+		return (paste("Your first string is longer by", length.difference, "characters"))
+	} else {
+	length.difference <- (length.string.2 - length.string.1)
+	return (paste("Your second string is longer by", length.difference, "characters"))
+	}
+}
+
 
 # Pass two strings of different lengths to your `DescribeDifference` function
+
+DescribeDifference("cat", "mice")
